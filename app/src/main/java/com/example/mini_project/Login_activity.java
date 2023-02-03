363587ffed27f74a0f4377509a110fe7a9815ac0
Sambhav Kothari <sambhavkothari2703@gmail.com>
2023-02-04 03:06:41 +0530

    firebase edit

--- app/src/main/java/com/example/mini_project/Login_activity.java
+++ app/src/main/java/com/example/mini_project/Login_activity.java
@@ -1,24 +1,30 @@
 package com.example.mini_project;
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login_activity extends AppCompatActivity {
 
-    EditText editText, editText2;
+    EditText editText, editText2, enter_email, enter_pass;
     String email, pass;
     Button btnInput;
+    FirebaseAuth mAuth;
 
+    @SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Intent intent2 = getIntent();
+        Intent intent1 = getIntent();
 
         btnInput = findViewById(R.id.button);
         btnInput.setOnClickListener(view -> {
@@ -29,7 +35,7 @@ public class Login_activity extends AppCompatActivity {
             editText2 = findViewById(R.id.enter_pass);
             pass = editText2.getText().toString();
 
-            if (email.equals("123@mit.com") && pass.equals("0000")) {
+            /*if (email.equals("123@mit.com") && pass.equals("0000")) {
                 Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
                 //toast is used to show message when clicking the button
 
@@ -39,6 +45,36 @@ public class Login_activity extends AppCompatActivity {
             else {
                 Toast.makeText(getApplicationContext(), "Invalid email or password", Toast.LENGTH_SHORT).show();
                 //toast is used to show message when clicking the button
+            }*/
+
+            enter_email = findViewById(R.id.enter_email);
+            enter_pass = findViewById(R.id.enter_pass);
+
+            mAuth = FirebaseAuth.getInstance();
+
+            if(mAuth.getCurrentUser() != null){
+                Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                //toast is used to show message when clicking the button
+
+                Intent intent2 = new Intent(getApplicationContext(), HomePage_activity.class);
+                startActivity(intent2);
+            }
+            else{
+                mAuth.signInWithEmailAndPassword(enter_email.getText().toString(), enter_pass.getText().toString())
+
+                        .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                                //toast is used to show message when clicking the button
+
+                                Intent intent2 = new Intent(getApplicationContext(), HomePage_activity.class);
+                                startActivity(intent2);
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(), "Invalid email or password", Toast.LENGTH_SHORT).show();
+                                //toast is used to show message when clicking the button
+                            }
+                        });
             }
 
         });
--- app/src/main/java/com/example/mini_project/Signin_activity.java
+++ app/src/main/java/com/example/mini_project/Signin_activity.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Signin_activity extends AppCompatActivity {
 
+    EditText enter_name, enter_age, enter_gender, enter_email, enter_pass;
+    String name, age, gender, email, pass;
     Button btnInput;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +27,38 @@ public class Signin_activity extends AppCompatActivity {
 
         btnInput = findViewById(R.id.button2);
         btnInput.setOnClickListener(view -> {
-            Toast.makeText(getApplicationContext(), "Welcome", Toast.LENGTH_SHORT).show();
-            //toast is used to show message when clicking the button
 
-            //Intent intent = new Intent(getApplicationContext(), HomePage.class);
-            //startActivity(intent);
+            enter_name = findViewById(R.id.enter_name);
+            name = enter_name.getText().toString();
+            enter_age = findViewById(R.id.enter_age);
+            age = enter_age.getText().toString();
+            enter_gender = findViewById(R.id.enter_gender);
+            gender = enter_gender.getText().toString();
+            enter_email = findViewById(R.id.enter_email);
+            email = enter_email.getText().toString();
+            enter_pass = findViewById(R.id.enter_pass);
+            pass = enter_pass.getText().toString();
+
+            mAuth = FirebaseAuth.getInstance();
+
+            mAuth.signInWithEmailAndPassword(email, pass)
+
+                    .addOnCompleteListener(this, task ->
+                            mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this, task2 -> {
+                        if (task.isSuccessful()){
+                            if(task2.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                                Toast.makeText(getApplicationContext(), "Welcome, Please Login", Toast.LENGTH_SHORT).show();
+
+                                Intent intent2 = new Intent(getApplicationContext(), Login_activity.class);
+                                startActivity(intent2);
+                            }
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Something went wrong, Try again", Toast.LENGTH_SHORT).show();
+                        }
+                    }));
         });
     }
 }
